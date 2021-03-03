0395554f0b6da0dfd12b9843641f6311b292e086
DashaNalivayko <dashanalivayko@gmail.com>
2021-03-03 14:45:17 +0300

    init: create the function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 
 // You should implement your task here.
-
+let arr = [];
 module.exports = function towelSort (matrix) {
-  return [];
-}
+  if (matrix && matrix.length) {
+    for (let i = 0; i < matrix.length; i++) {
+      if (i % 2 !== 0) {
+        matrix[i].reverse() 
+      }
+      arr = matrix.toString().split(",").map(val => parseInt(val)); 
+    }
+    return  arr
+  } else {
+    return []
+  }
+ 
+};
